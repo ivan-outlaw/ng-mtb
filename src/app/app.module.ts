a7c40928cd9245ec4757b2dd7e3cedbeca8d5e8e
vvivan <outlaw_serni@yahoo.com>
2017-05-13 22:30:32 +0200

    Header and Navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './page-elements/navbar/navbar.component';
+import { FooterComponent } from './page-elements/footer/footer.component';
+import { HeaderComponent } from './page-elements/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
