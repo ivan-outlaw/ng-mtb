ae0a0177be34e7f1144a3cedae071e9211d9b74a
misaizdaleka <milos.boskovic@gmail.com>
2017-05-24 20:34:13 +0200

    Make header show only on the main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My Bootstrapped Angular app works!!! Yeahh !';
+
+  constructor(private location: Location) {
+  }
+
+  isActiveRoute(route: string) {
+    return location.pathname.indexOf(route) > -1;
+  }
 }
